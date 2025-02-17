fda71cd99a0ce9f3b18f93e633d9af2b97aa7f73
cfoster5 <cfoster3204@gmail.com>
2025-02-17 10:24:06 -0600

    refactor: Update circle and icon sizes; Remove dead code

--- app/index.tsx
+++ app/index.tsx
@@ -10,56 +10,45 @@ import {
 
 import { ThemedView } from "@/components/ThemedView";
 import { useIntakeStore } from "@/stores/store";
-import { SymbolView } from "expo-symbols";
+import { SFSymbol, SymbolView } from "expo-symbols";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { iOSColors, iOSUIKit } from "react-native-typography";
 import { useLocales } from "expo-localization";
 
+type CircleButtonProps = {
+  handlePress: () => void;
+  symbolName: SFSymbol;
+};
 
-const Button = ({ handlePress, symbolName, style }) => (
-  <Pressable
-    onPress={handlePress}
-    style={[
-      {
-        backgroundColor: iOSColors.blue,
-        minWidth: 44,
-        minHeight: 44,
-        borderRadius: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      style,
-    ]}
-  >
-    <SymbolView
-      name={symbolName}
-      size={iOSUIKit.bodyObject.fontSize}
-      tintColor={iOSColors.white}
-    />
-  </Pressable>
-);
+const Button = ({ handlePress, symbolName }: CircleButtonProps) => {
+  const { width } = useWindowDimensions();
+  return (
+    <Pressable
+      onPress={handlePress}
+      style={[
+        {
+          backgroundColor: iOSColors.blue,
+          minWidth: width * (1 / 6),
+          minHeight: width * (1 / 6),
+          borderRadius: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+      ]}
+    >
+      <SymbolView
+        name={symbolName}
+        size={(width * (1 / 6)) / 2.5}
+        tintColor={iOSColors.white}
+      />
+    </Pressable>
+  );
+};
 
 export default function NotFoundScreen() {
   const { width } = useWindowDimensions();
   const [locale] = useLocales();
-  const screenPadding = 96;
-  const circleHeight = width - screenPadding * 2;
-  // const [entryAmount, setEntryAmount] = useState(8);
-
   const { addEntry, entries, resetDailyEntries } = useIntakeStore();
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      // alignItems: "center",
-      // justifyContent: "center",
-      // padding: screenPadding,
-    },
-    link: {
-      marginTop: 15,
-      paddingVertical: 15,
-    },
-  });
 
   const currentDate = new Date().toLocaleDateString();
 
@@ -98,7 +87,7 @@ export default function NotFoundScreen() {
             >
               <SymbolView
                 name="list.bullet"
-                size={iOSUIKit.bodyObject.fontSize}
+                size={iOSUIKit.bodyObject.lineHeight}
                 tintColor={iOSColors.white}
               />
             </Pressable>
@@ -110,84 +99,84 @@ export default function NotFoundScreen() {
             flexGrow: 1,
             alignItems: "center",
             justifyContent: "center",
-            padding: screenPadding,
           }}
         >
           <Pressable
             style={{
               backgroundColor: iOSColors.blue,
-              width: "100%",
-              height: circleHeight,
+              width: width * (1 / 2),
+              height: width * (1 / 2),
               borderRadius: "100%",
               alignItems: "center",
               justifyContent: "center",
             }}
           >
-            {/* <Text style={iOSUIKit.bodyWhite}>You've had</Text> */}
             <Text style={iOSUIKit.largeTitleEmphasizedWhite}>
               {totalAmount}
               {locale.measurementSystem === "metric" ? "ml" : "oz"}
             </Text>
             <Text style={iOSUIKit.bodyWhite}>today</Text>
           </Pressable>
-          {/* </View> */}
-            <SafeAreaView
+          <SafeAreaView
             style={{
               flexDirection: "row",
               justifyContent: "space-evenly",
               width: "100%",
             }}
-            >
+          >
             <Button
               handlePress={() => {
-              Alert.alert(
-                "Reset Today's Entries?",
-                "Are you sure you want to reset today's entries?",
-                [
-                {
-                  text: "Cancel",
-                  style: "cancel",
-                },
-                {
-                  text: "OK",
-                  onPress: resetDailyEntries,
-                },
-                ]
-              );
+                Alert.alert(
+                  "Reset Today's Entries?",
+                  "Are you sure you want to reset today's entries?",
+                  [
+                    {
+                      text: "Cancel",
+                      style: "cancel",
+                    },
+                    {
+                      text: "OK",
+                      onPress: resetDailyEntries,
+                    },
+                  ]
+                );
               }}
               symbolName="arrow.clockwise"
             />
             <Button
               handlePress={() => {
-              Alert.prompt(
-                "Enter Amount",
-                null,
-                [
-                {
-                  text: "Cancel",
-                  style: "cancel",
-                },
-                {
-                  text: "OK",
-                  onPress: (amount) => {
-                  const numericAmount = parseInt(amount, 10);
-                  if (!isNaN(numericAmount)) {
-                    // setEntryAmount(numericAmount);
-                    handleAddEntry(numericAmount);
-                  }
-                  },
-                },
-                ],
-                "plain-text",
-                "",
-                "numeric"
-              );
+                Alert.prompt(
+                  "Enter Amount",
+                  null,
+                  [
+                    {
+                      text: "Cancel",
+                      style: "cancel",
+                    },
+                    {
+                      text: "OK",
+                      onPress: (amount) => {
+                        const numericAmount = parseInt(amount, 10);
+                        if (!isNaN(numericAmount)) {
+                          handleAddEntry(numericAmount);
+                        }
+                      },
+                    },
+                  ],
+                  "plain-text",
+                  "",
+                  "numeric"
+                );
               }}
               symbolName="plus"
             />
-            </SafeAreaView>
+          </SafeAreaView>
         </View>
       </ThemedView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
