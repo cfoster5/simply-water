67bbe05c4c46811d72a3bb6ff1f7e09d13c33095
cfoster5 <cfoster3204@gmail.com>
2025-02-18 13:25:26 -0600

    chore: Convert app config to dynamic js file; Add Google services file and RN Firebase plugins

---
+++ app.config.js
@@ -0,0 +1,66 @@
+export default {
+  expo: {
+    name: "Simply Water",
+    slug: "water",
+    version: "1.1.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.cfoster.water",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_PLIST ?? "./GoogleService-Info.plist",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.cfoster.water",
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      "expo-localization",
+      "@react-native-firebase/app",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "f702d13d-0324-49e1-9db5-794e71956f21",
+      },
+    },
+  },
+};
